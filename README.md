a1e1f5c1fb7371621364a5e084663f5c75bb90f3
klangner <klangner@gmail.com>
2017-05-30 23:02:05 +0200

    Demo cart-pole environment using random policy

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # OpenAI Gym examples
 
 The folders are related to OpenAI Gym categories.
+
+For more check [OpenAI Gym](https://gym.openai.com)
--- classic/cart_pole.py
+++ classic/cart_pole.py
@@ -3,8 +3,30 @@
 import gym
 
 
-env = gym.make('CartPole-v1')
-env.reset()
-for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample()) # take a random action
+class RandomAgent(object):
+    """The world's simplest agent!"""
+    def __init__(self, action_space):
+        self.action_space = action_space
+
+    def act(self, observation, reward, done):
+        return self.action_space.sample()
+
+
+if __name__ == '__main__':
+    episode_count = 100
+    env = gym.make('CartPole-v0')
+    agent = RandomAgent(env.action_space)
+
+    for i in range(episode_count):
+        ob = env.reset()
+        env.render()
+        reward = 0
+        total_reward = 0
+        done = False
+        while not done:
+            action = agent.act(ob, reward, done)
+            ob, reward, done, _ = env.step(action)
+            total_reward += reward
+        print('Episode: {}, reward={}'.format(i+1, total_reward))
+
+    env.close()

5fedda56037a6613fc55411d120776b24998c759
klangner <klangner@gmail.com>
2019-04-15 21:34:09 +0200

    Move OpenAI gym solutions to its own folder

--- open-ai-gym/cart-pole/dqn/network.py
+++ open-ai-gym/cart-pole/dqn/network.py
@@ -1,16 +1,9 @@
-#
-# Various QNetwork architectures used in different scenarios
-#
-
-import numpy as np
-import gym
-from keras import models, layers, optimizers
+import  numpy as np
+from tensorflow.keras import models, layers, optimizers
 
 
-# ----------------------------------------------------------------------------------------------------------------------
-# Define QNetwork
 class QNetwork:
-
+    
     def __init__(self, input_shape, n_actions, alpha=0.0003):
         self.input_shape = input_shape
         self.output_shape = n_actions
@@ -25,34 +18,23 @@ class QNetwork:
         opt = optimizers.RMSprop(lr=self.alpha)
         model.compile(loss='mse', optimizer=opt)
         return model
-
+    
     def predict(self, state):
         """Make prediction for single state and return q values for all actions"""
         s = np.expand_dims(state, axis=0)
         return self.model.predict(s)[0]
-
+    
     def predict_batch(self, states):
         """Make prediction for list of states"""
         return self.model.predict(states)
-
+    
     def train(self, x, y):
         self.model.fit(x, y, batch_size=64, verbose=0)
 
 
-# ----------------------------------------------------------------------------------------------------------------------
-# Tests
-GREEN = '\033[92m'
-RED = '\033[91m'
-ENDC = '\033[0m'
-
-
-def tests_qnetwork():
-    """Check if QNetwork will compile"""
+if __name__ == "__main__":
+    import gym
     env = gym.make("CartPole-v0")
     network = QNetwork(env.observation_space.shape, env.action_space.n)
     network.predict(env.reset())
-    print(GREEN + 'QNetwork ok!' + ENDC)
-
-
-if __name__ == "__main__":
-    tests_qnetwork()
+    print('QNetwork ok!')
